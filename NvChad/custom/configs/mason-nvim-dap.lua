15d782e9628c765e4c495514a0bf4c400d9c299f
zhoukaiye <zhoukaiye@xiaomi.com>
2023-05-01 21:05:20 +0800

    Improve ui for dap.

---
+++ NvChad/custom/configs/mason-nvim-dap.lua
@@ -0,0 +1,50 @@
+local M = {}
+
+-- ***********************************************************************
+
+M.ensure_installed = { "python", "delve" }
+
+-- ***********************************************************************
+-- *                         PYTHON DEBUGGING                            *
+-- ***********************************************************************
+
+python_opts = function(source_name)
+    local dap = require "dap"
+    dap.adapters.python = {
+      type = "executable",
+      command = vim.fn.system('printf $(dirname "$(realpath "$(which debugpy)")")') .. '/venv/bin/python',
+      args = {
+        "-m",
+        "debugpy.adapter",
+      },
+    }
+    dap.configurations.python = {
+      {
+        type = "python",
+        request = "launch",
+        name = "Launch file",
+        program = "${file}", -- This configuration will launch the current file if used.
+        pythonPath = function()
+          -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+          -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+          -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+          local currentpythonpath=vim.fn.system('printf $(which python)')
+          return currentpythonpath
+        end
+      },
+    }
+  end
+
+
+-- ***********************************************************************
+-- *                         C/CPP DEBUGGING                             *
+-- ***********************************************************************
+
+
+-- ***********************************************************************
+
+M.handlers = {
+  python = python_opts,
+}
+
+return M
--- NvChad/custom/configs/nvim-dap.lua
+++ NvChad/custom/configs/nvim-dap.lua
@@ -5,36 +5,53 @@ if not present then
 end
 
 -- adapter
-dap.adapters.python = {
-  type = 'executable';
-  command = '/home/zky/Software/anaconda/anaconda3/envs/debugpy/bin/python';
-  args = { '-m', 'debugpy.adapter' };
-}
+-- dap.adapters.python = {
+--   type = 'executable';
+--   command = '/home/zky/Software/anaconda/anaconda3/envs/debugpy/bin/python';
+--   args = { '-m', 'debugpy.adapter' };
+-- }
 
--- configuration
-local dap = require('dap')
+
+-- ***********************************************************************
+-- *                         PYTHON DEBUGGING                            *
+-- ***********************************************************************
+
+dap.adapters.python = {
+  type = "executable",
+  -- command = vim.fn.system('printf $(dirname "$(realpath "$(which debugpy)")")') .. '/venv/bin/python',
+  command = '/home/zky/Software/anaconda/anaconda3/envs/debugpy/bin/python';
+  args = { "-m", "debugpy.adapter", },
+}
 dap.configurations.python = {
   {
     -- The first three options are required by nvim-dap
     type = 'python'; -- the type here established the link to the adapter definition: `dap.adapters.python`
     request = 'launch';
     name = "Launch file";
-
-    -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
-
     program = "${file}"; -- This configuration will launch the current file if used.
     pythonPath = function()
       -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
       -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
       -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
-      local currentpythonpath=vim.fn.system('printf $(which python)')
+      local currentpythonpath=vim.fn.system('printf $(which python3)')
       return currentpythonpath
     end;
   },
 }
 
+-- ***********************************************************************
+-- *                         GENERAL DEBUGGING                           *
+-- ***********************************************************************
+
+-- set sign for dap
+vim.fn.sign_define('DapBreakpoint', {text='', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapBreakpointCondition', {text='', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapBreakpointRejected', {text='', texthl='', linehl='', numhl=''})
 
 
+-- ***********************************************************************
+-- *                          DAP UI CONFIG                              *
+-- ***********************************************************************
 
 local present, dapui = pcall(require, "dapui")
 
